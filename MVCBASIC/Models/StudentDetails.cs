6b33c320f430e8bdf1ea09d0eba435fc099e9733
LAPTOP-1SE8I7RI\venkyk <vkatragadda95@gmail.com>
2018-04-12 10:07:01 -0400

    Commit to other branch

---
+++ MVCBASIC/Models/StudentDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCBASIC.Models
+{
+    public class StudentDetails
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Country { get; set; }
+        public string States { get; set; }
+        public string Gender { get; set; }
+        public bool Student { get; set; }
+        public bool working { get; set; }
+        public string Address { get; set; }
+        public DateTime DoB { get; set; }
+        public int Age { get; set; }
+    }
+}
