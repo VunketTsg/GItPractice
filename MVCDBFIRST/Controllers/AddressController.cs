dc755de6c33d31f933ba9411b6ffffe70f3cef99
LAPTOP-1SE8I7RI\venkyk <vkatragadda95@gmail.com>
2018-04-12 09:39:37 -0400

    3rd commit

---
+++ MVCDBFIRST/Controllers/AddressController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDBFIRST.Controllers
+{
+    public class AddressController : Controller
+    {
+        // GET: Address
+        public ActionResult GetData()
+        {
+            return View();
+        }
+
+        public JsonResult GetCountries()
+        {
+            List<String> lstcntry = new List<string>()
+            {
+                "USA","IND","UK"
+            };
+
+            return Json(lstcntry, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetStates(string countryName)
+        {
+            List<string> lstStates = new List<string>();
+            switch(countryName)
+            {
+                case "USA":
+                    {
+                        lstStates.Add("NY");
+                        lstStates.Add("MA");
+                        lstStates.Add("CA");
+                        break;
+                    }
+                case "IND":
+                    {
+                        lstStates.Add("TS");
+                        lstStates.Add("AP");
+                        lstStates.Add("MP");
+                        break;
+                    }
+                case "UK":
+                    {
+                        lstStates.Add("Lords");
+                        lstStates.Add("Oval");
+                        lstStates.Add("London");
+                        break;
+                    }
+                default:
+                    {
+                        break;
+                    }
+            }
+            return Json(lstStates);
+        }
+
+    }
+}
---
+++ MVCDBFIRST/Controllers/EmployeeInfoController.cs
@@ -0,0 +1,39 @@
+﻿using MVCDBFIRST.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDBFIRST.Controllers
+{
+    public class EmployeeInfoController : Controller
+    {
+
+        EmployeeInfoContext db = new EmployeeInfoContext();
+
+        public ActionResult Index()
+        {
+            return View(db.Employees.ToList());
+        }
+
+        [HttpGet]
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Employee employee)
+        {
+            if(ModelState.IsValid)
+            {
+                db.Employees.Add(employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+    }
+}
